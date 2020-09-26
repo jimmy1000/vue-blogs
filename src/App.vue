63a6665af52b1c822c9a36256b624f2b152ddddc
jimmy599829203@gmail.com <jimmy599829203@gmail.com>
2020-09-27 05:42:07 +0800

    vue-cli

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<!-- <AddBlogs></AddBlogs>
+	<Show-Blogs></Show-Blogs> -->
+	<BlogHeader></BlogHeader>
+	<router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BlogHeader from './components/BlogHeader.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+	BlogHeader
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,48 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import Routes from './components/router/Router'
 
 Vue.config.productionTip = false
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+//全局自定义指令
+/* Vue.directive('rainbow', {
+  bind(el) {
+    el.style.color = '#' + Math.random().toString(16).slice(2,8);
+  }
+});*/
+Vue.directive('theme', {
+  bind(el,binding) {
+	if (binding.value == 'wide') {
+		el.style.maxWidth = "1260px";
+	}else if(binding.value == 'narrow'){
+		el.style.maxWidth = "860px";
+	}
+	if(binding.arg == 'column') {
+		el.style.background = "#6677cc";
+	}
+	}
+}); 
+
+
+/*    全局过滤   
+Vue.filter("to-uppercase",function(value){
+	return value.toUpperCase();
+}) */
+Vue.filter("snippet",function(value){
+	return value.slice(0,100) + "...";
+})
+
+//
+const router = new VueRouter({
+	routes:Routes,
+	mode:"history"
+})
 
 new Vue({
+	router,
   render: h => h(App),
 }).$mount('#app')
